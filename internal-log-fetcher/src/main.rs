19d3d542a0ce60a048754386222304bac5f6b598
Adrian Nagy <adrian.nagy@viablesystems.io>
2023-05-19 13:44:14 +0200

    Add option to provide full url for graphql

--- internal-log-fetcher/src/main.rs
+++ internal-log-fetcher/src/main.rs
@@ -6,17 +6,15 @@ use structopt::StructOpt;
 
 mod authentication;
 mod graphql;
-mod mina_server;
 mod log_entry;
+mod mina_server;
 mod utils;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Internal logging fetcher.")]
 struct Opts {
-    #[structopt(name = "NODE_ADDRESS")]
-    address: String,
-    #[structopt(short = "p", long)]
-    graphql_port: u16,
+    #[structopt(subcommand)]
+    target: Target,
     #[structopt(short = "k", long, parse(from_os_str))]
     secret_key_path: PathBuf,
     #[structopt(short = "o", long, parse(from_os_str))]
@@ -25,13 +23,28 @@ struct Opts {
     https: bool,
 }
 
+#[derive(Debug, StructOpt)]
+enum Target {
+    #[structopt(about = "Specify Node address and port separately")]
+    NodeAddressPort {
+        #[structopt(name = "NODE_ADDRESS")]
+        address: String,
+        #[structopt(short = "p", long)]
+        graphql_port: u16,
+    },
+    #[structopt(about = "Specify full URL")]
+    FullUrl {
+        #[structopt(name = "URL")]
+        url: String,
+    },
+}
+
 fn main() {
     let opts = Opts::from_args();
     let secret_key_base64 = std::fs::read_to_string(opts.secret_key_path).unwrap();
     let config = mina_server::MinaServerConfig {
         secret_key_base64,
-        address: opts.address,
-        graphql_port: opts.graphql_port,
+        target: opts.target,
         output_dir_path: opts.output_dir_path,
         use_https: opts.https,
     };
